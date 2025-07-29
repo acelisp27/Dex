00e08affb33124e707c38f0289830c7d8fd083d1
AndresCelis <celisp27@outlook.com>
2025-07-29 11:13:35 -0500

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Andres Celis on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
