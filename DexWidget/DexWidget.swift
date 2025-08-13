5ba40060a4d85ebcd4a5f3a4cde171c86f93a011
AndresCelis <celisp27@outlook.com>
2025-08-13 11:32:06 -0500

    [Feature] design three pokemon widgets

--- DexWidget/DexWidget.swift
+++ DexWidget/DexWidget.swift
@@ -24,7 +24,7 @@ struct Provider: TimelineProvider {
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            _ = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
             let entry = SimpleEntry.placeHolder
             entries.append(entry)
         }
@@ -52,36 +52,109 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct DexWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        VStack {
-            entry.sprite
-            
+    @Environment(\.widgetFamily) var widgetSize
+    
+    var pokemonImage: some View {
+        entry.sprite
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .shadow(color: .black, radius: 6)
+    }
+    
+    var typesView: some View {
+        ForEach(entry.types, id: \.self) { type in
+            Text(type.capitalized)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+                .padding(.horizontal, 13)
+                .padding(.vertical, 5)
+                .background(Color(type.capitalized))
+                .clipShape(.capsule)
+                .shadow(radius: 3)
         }
     }
+        
+    var entry: Provider.Entry
+        
+    var body: some View {
+        switch widgetSize {
+        case .systemMedium:
+            HStack {
+                pokemonImage
+                Spacer()
+                VStack (alignment: .leading){
+                    Text(entry.name.capitalized)
+                        .font(.title)
+                        .padding(.vertical, 1)
+                    HStack {
+                        typesView
+                    }
+                }
+                .layoutPriority(1)
+            }
+        case .systemLarge:
+            ZStack {
+                pokemonImage
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.largeTitle)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.75)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        typesView
+                    }
+                }
+            }
+        default:
+            pokemonImage
+        }
+    
+    
+        }
 }
 
 struct DexWidget: Widget {
     let kind: String = "DexWidget"
 
+
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 DexWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                    .foregroundStyle(.black)
+                    .containerBackground(Color(entry.types[0].capitalized), for: .widget)
             } else {
                 DexWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Pokemon")
+        .description("See a random Pokemon")
     }
 }
-
-#Preview(as: .systemSmall) {
+//
+//#Preview(as: .systemSmall) {
+//    DexWidget()
+//} timeline: {
+//    SimpleEntry.placeHolder
+//    SimpleEntry.placeholder2
+//}
+//
+//#Preview(as: .systemMedium) {
+//    DexWidget()
+//} timeline: {
+//    SimpleEntry.placeHolder
+//    SimpleEntry.placeholder2
+//}
+//
+#Preview(as: .systemLarge) {
     DexWidget()
 } timeline: {
     SimpleEntry.placeHolder
